b05870b1525a400526ec9eb5c7631fce991f4b4e
Govind Mishra <govindmishra102005@gmail.com>
2025-05-15 17:18:29 +0000

    under keyword

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,15 @@
+let name = "Govind ji" 
+const id = "2023b0101204"
+var sub = "math"
+
+// id = "2023b0101205"  show error bcz value cant be changed if once declare 
+
+name = "govind"
+sub = "java"
+
+/* var is not used nowadays 
+ because of local and functional scope 
+*/
+
+
+ console.table([name, id, sub]) 
