433c0d734dbc60142596784b336f3a27aef0e746
Govind Mishra <govindmishra102005@gmail.com>
2025-05-19 14:58:29 +0000

    learn stack and heap

--- 01_basic/datatypes_summary.js
+++ 01_basic/datatypes_summary.js
@@ -1,5 +1,8 @@
 // Primitve 
 
+ 
+
+
 // 7 types : Number , String , Boolean , Null , Undefined , Symbol , BigInt 
 
 
@@ -14,10 +17,34 @@ const objVar = {
     marks : 90
 }
 
-const funcVar = function() {
-    console.log("Hello World")
-}
+// const funcVar = function() {
+//     console.log("Hello World")
+// }
 
-console.log(typeof funcVar)
-console.log(typeof objVar) 
-console.log(typeof sub)
+// console.log(typeof funcVar)
+// console.log(typeof objVar) 
+// console.log(typeof sub)
+
+ // ****************** Memory **************************
+
+// Stack (Primitive), Heap(Non - Primitive) 
+
+let nameOne = "Govind Mishra" 
+let nameTwo = nameOne ; 
+nameTwo = "Govind"
+console.log(nameOne)
+console.log(nameTwo)
+
+// stack create assign copy to new variable 
+// but in heap memory variable reference to data 
+
+const clgData1 = {
+    id : "abc123",
+    name : "Govind"
+}
+const clgData2 =  clgData1 
+clgData2.id = "abc456"
+console.log(clgData1.id)
+console.log(clgData2.id)
+
+ 
