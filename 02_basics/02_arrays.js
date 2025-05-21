0c6a2d18029ea75cd32b1facb90713f23b13f620
Govind Mishra <govindmishra102005@gmail.com>
2025-05-21 20:12:28 +0000

    learn how to handle data in array

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,20 @@
+let arr1 = [1,2,3] 
+let arr2 = [4,5,6] 
+// arr1.push(arr2)
+// console.log(arr1)
+// console.log(arr2) ;  
+
+// const arr3 = arr1.concat(arr2) ; 
+// console.log(arr3) ; 
+
+// const arr4 = [...arr1, ...arr2, 10] ; 
+// console.log(arr4) ; 
+
+// console.log(Array.isArray("govind")) ; 
+// console.log(Array.from("123")) ; 
+// console.log(Array.from({name : "Govind"})) ; // ************  dont change key value to direclty array so return empty array 
+
+// let score1 = 100 ; 
+// let sc2 = 400 
+// let sc3 = 900 
+// console.log(Array.of(score1, sc2,sc3))
