730a348ddf3aab42e2c1e039874d86154e7fc923
Govind Mishra <govindmishra102005@gmail.com>
2025-05-24 00:49:13 +0530

    refactor: comment out initial variable declarations and functions for clarity

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,12 +1,40 @@
-let a = 10 
-const b = 20 
-var c = 40  // global access , use throughot the program 
+// let a = 10 
+// const b = 20 
+// var c = 40  // global access , use throughot the program 
 
-function printNum(){
-    var c = 400 
-    let a = 40 
-    console.log(`in funtion ${a}`);
+// function printNum(){
+//     var c = 400 
+//     let a = 40 
+//     console.log(`in funtion ${a}`);
     
+// }
+// printNum() 
+// console.log(a)
+
+//  ************ closure concept 
+
+// function one () {
+//     const name = "Govind"
+//     function two () {
+//         const website = "youtube" 
+//         console.log(name); // name can be access 
+        
+//     }
+//      console.log(website) not defined show error  
+//     two()
+// }
+// one () 
+
+// ***** interesting and imp for interview 
+addOne(4)  // it will not show error bcz function can access 
+function addOne (num) {
+    return num + 1 
 }
-printNum() 
-console.log(a)
+
+console.log(addTwo(4)) // it will show error bcz addTwo is not initilization 
+const addTwo = function (num) {
+    return num + 2
+}
+ 
+
+
