312dc82bced83da59dc3612f9a9d93b9cbd58b8a
Govind Mishra <govindmishra102005@gmail.com>
2025-05-27 00:36:26 +0530

    for loop with break statement

---
+++ 05_iteration/one.js
@@ -0,0 +1,11 @@
+// for 
+const array = [0,1,2,3,4]
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+// }
+for (let index = 0; index < array.length; index++) {
+    if(index == 3) break ; 
+    const element = array[index];
+    console.log(element);   
+}
+
