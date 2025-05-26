60cee0cd79ff78b23c6386480b44b8fb7d7e7ba2
Govind Mishra <govindmishra102005@gmail.com>
2025-05-27 00:22:00 +0530

    add: implement switch statement examples for month handling

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,15 @@
+const month = 2 
+switch (month) {
+    case 1 : 
+        console.log("January") ; 
+        break ; 
+    case 2 : 
+        console.log("February") ; 
+        break ;     
+    case "april" : 
+        console.log("March") ; 
+        break ; 
+    default : 
+        console.log("default case match");
+            
+}
