eb8a05b2a9a442d753aa78d92515c316e6be0827
Govind Mishra <govindmishra102005@gmail.com>
2025-05-27 00:22:30 +0530

    add: create examples for truthy and falsy values, nullish coalescing, and ternary operator

---
+++ 04_control_flow/03_truthy.js
@@ -0,0 +1,40 @@
+// const usermail = [] 
+// const usermail = "govind@email.com" 
+// const usermail = "" 
+
+
+// if(usermail){
+//     console.log("Got user email");
+// }else {
+//     console.log("Dont have user email")
+// }
+
+// ****** falsy value 
+
+// false , 0 , -0 , BigInt 0n, "", null, undefined, Nan 
+
+// ***** truthy value 
+
+// "0", 'false' , " ", [] , {} , function(){}
+
+// const emptyObj = {} 
+
+// if(Object.keys(emptyObj).length === 0){
+//     console.log("Object is empty");
+// }
+
+// ****** Nullish Coalescing Operator (??) : null ,undefined 
+
+let val1 ;
+// val1 = 5 ?? 10 ; 
+// val1 = null ?? 10 ; 
+// val1 = undefined ?? 15 ; 
+// console.log(val1);
+
+// ****** Terniary operator 
+// condition ? ture : false 
+
+const vote = 100 
+vote >= 500 ? console.log("win") : console.log("loss");
+
+
