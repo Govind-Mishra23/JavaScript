42c3910d08c5e2dc56b5d4a34fca150750f2fb51
Govind Mishra <govindmishra102005@gmail.com>
2025-05-27 00:53:24 +0530

    learn while and do while

---
+++ 05_iteration/two.js
@@ -0,0 +1,14 @@
+// let i = 0 ; 
+
+// do {
+//     i += 1 
+//     console.log(`${i}`)
+// } while (i<=5) ;
+
+let i = 5
+do {
+    console.log(i++) 
+} while (i<5){
+    console.log("condition voileted");
+    
+}
