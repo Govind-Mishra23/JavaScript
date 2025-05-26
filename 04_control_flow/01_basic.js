90aa5f02ca44953d862167665f655817f2455f57
Govind Mishra <govindmishra102005@gmail.com>
2025-05-26 23:55:31 +0530

    add: create initial control flow examples with conditional statements

---
+++ 04_control_flow/01_basic.js
@@ -0,0 +1,17 @@
+// if
+
+// if(true){
+//     console.log("true condition");
+// }
+
+// if(3!=2) console.log("yes"), console.log("poor readibility") ; // implicit type scope 
+
+let balance = 1000 
+if(balance > 500) {
+    console.log("rich")
+}else if (balance > 400){
+    console.log("mayberich") 
+}else {
+    console.log("need hard work");
+    
+}
