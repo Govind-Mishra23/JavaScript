573a0b2ab13e3d3381f50016c957000dbc659a5f
Govind Mishra <govindmishra102005@gmail.com>
2025-05-28 00:58:57 +0530

    feat: add initial implementation of forEach examples for arrays and objects

---
+++ 05_iteration/five.js
@@ -0,0 +1,43 @@
+const programming = ["java", "javascript", "python", "ruby"]
+
+// programming.forEach(function (language) {
+//     console.log(language)
+// })
+
+
+// **** use arrow function 
+// programming.forEach((lang) => {
+//     console.log(lang)
+// })
+
+
+// ***** pass direct function in forEach
+// function printMe(item){
+//     console.log(item)
+// }
+// programming.forEach(printMe) // printMe is referece for function 
+
+programming.forEach((lang, index, arr) => {
+    // console.log(lang, index, arr)
+})
+
+// ******** iteration on object which is store in array 
+
+const details = [
+    {
+        langName : "java",
+        ques : 300
+    },
+    {
+        langName : "python",
+        ques : 200
+    },
+    {
+        langName : "javaScript",
+        ques : 250
+    }
+]
+details.forEach((item) => {
+    // console.log(item.langName )
+})
+
